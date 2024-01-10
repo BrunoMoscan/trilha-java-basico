9e84f8aaf5ad6ba041abba643b903498ec20b49b
BrunoMoscan <bruno.moscan@icloud.com>
2024-01-10 19:57:57 -0300

    Teste Pulando e Escrevendo nomes. Versão 1.

---
+++ LendoPulandoNomes/LendoPulandoNomes.java
@@ -0,0 +1,19 @@
+package LendoPulandoNomes;
+
+import java.util.*;
+
+public class LendoPulandoNomes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String [] nome = new String[10];
+		
+		for(int i=0 ; i<10 ; i++){
+			nome[i] = sc.nextLine();
+        }
+        System.out.println(nome[2]);
+        System.out.println(nome[6]);
+        System.out.println(nome[8]);
+	}
+
+ 
+}
