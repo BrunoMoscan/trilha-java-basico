bb8993baa5cf5f01c5bfa969e62af91573b029ab
BrunoMoscan <bruno.moscan@icloud.com>
2024-01-19 12:20:50 -0300

    git commit

---
+++ Digital Bank/src/Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+    private String nome;
+    private int cpf;
+    private int telefone;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+ 
+
+}
